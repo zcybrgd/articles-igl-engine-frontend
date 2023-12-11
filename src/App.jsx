2fc72add9c1a5601e1008551388f58f9948b7a4d
Nwasler <107283332+NaDdjg@users.noreply.github.com>
2023-12-11 23:08:56 +0100

    styling the scrollBar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 export default function App() {
   return (
     <BrowserRouter>
-      <Router user={true} userRole={"moderator"} />
+      <Router user={true} userRole={"client"} />
     </BrowserRouter>
   );
 }
