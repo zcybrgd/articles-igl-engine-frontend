5ff48c688fdb79c809667026eef9f9d703704227
Nwasler <107283332+NaDdjg@users.noreply.github.com>
2023-12-11 16:51:40 +0100

    tailwind css configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
