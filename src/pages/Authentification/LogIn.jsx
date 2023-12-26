1e6e2abb2bd1665c6bafb9aa1c5d90b85d300f02
Nwasler <107283332+NaDdjg@users.noreply.github.com>
2023-12-27 00:24:36 +0100

    added styling to login and signup pages

--- src/pages/Authentification/LogIn.jsx
+++ src/pages/Authentification/LogIn.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react"
-import { BrowserRouter, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { logIn } from "../../services/authApi";
-import Router from "../Router";
 import { useAuth } from "../../context/AuthContext";
+import articleIcon from '../.././assets/icons/article.svg';
+import LoginAnimation from "../../assets/gifs/LoginAnimation.gif"
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -32,33 +33,31 @@ const LoginPage = () => {
         password: password,
       }
 
+      const response = await logIn(userData)
 
-       const response = await logIn(userData)
-
-       if (response && response.data && response.data.token) {
+      if (response && response.data && response.data.token) {
         setSuccessfulMessage(`successful login, ${username} ! gonna redirect you in a sec`)
-    
-      const userRole = response.data.user.role.toLowerCase();
-      const userDataAndRole = {
-        userName: username,
-        password: password,
-        userRole: userRole,
-      };
 
-      await login(userDataAndRole);
+        const userRole = response.data.user.role.toLowerCase();
+        const userDataAndRole = {
+          userName: username,
+          password: password,
+          userRole: userRole,
+        };
 
-      navigate(`/`);
-       } else {
-         if (response && response.data && response.data.error) {
-           setErreur(`Login failed. ${response.data.error}`);
+        await login(userDataAndRole);
+
+        navigate(`/`);
+      } else {
+        if (response && response.data && response.data.error) {
+          setErreur(`Login failed. ${response.data.error}`);
         }
-       }
+      }
     } catch (error) {
       console.error("Error in handleLogin:", error);
       setErreur("An unexpected error occurred.");
     }
   };
-
 
   const leftBorderRadius = {
     borderTopLeftRadius: '20px',
@@ -75,45 +74,39 @@ const LoginPage = () => {
 
   return (
     <div className='absolute top-0 right-0 left-0 bg-black grid grid-cols-2 max-sm:h-full'>
-
-      <div className='flex max-sm:flex'>
-        <div className='w-screen h-screen grid   max-sm:h-screen bg-black lg:w-4/5'>
-
-          <div className='w-full h-full  grid-rows-2 max-sm:h-full sm:hidden '>
-            <div className=" max-sm: text-white max-sm: font-bold  max-sm:text-5xl max-sm: mt-8 md:hidden lg:hidden max-md:text-5xl max-sm:ml-4 sm-hidden">
-              login
-            </div>
-            <div className=" max-sm: text-white max-sm: font-bold  max-sm:text-xl max-sm: mt-0 md:hidden sm-hidden lg:hidden max-md:text-5xl max-sm:ml-4">
-              Welocme back
+      <div className='flex'>
+        <div className='h-screen grid max-sm:h-screen bg-black lg:w-4/5'>
+          <div className="absolute top-1 left-1">
+            <div className="hidden max-sm:flex flex-col items-start justify-start p-5 space-y-2">
+              <h1 className="text-white bg-black text-4xl font-bold"> Login </h1>
+              <p className="text-white bg-black text-xl font-bold">Welcome back!</p>
             </div>
           </div>
-          <div className="absolute inset-y-0 left-16 w-3/5 md:w-2/5 h-full md:h-full flex flex-col justify-center items-center  max-sm:h-4/5 max-sm:mt-28">
-            <div className="flex flex-col sm:w-full items-center text-white space-y-2 ">
+          <div className="absolute inset-y-0 max-sm:left-8 justify-center items-center w-4/5 md:w-3/5 h-full md:h-full max-sm:h-4/5 max-sm:mt-28">
+            <div className="flex flex-col w-[100%] items-center justify-center text-white space-y-2 ">
               <div className="mb-12 "></div>
-              <div className=" absolute inset-y-8 w-5/5 h-5/5 place-content-center mt-16 space-y-0  md:bg-black lg:bg-black max-sm:bg-white max-sm:rounded-lg sm:bg-black sm:mb-0 ">
-                <div className="flex w-72 flex-col gap-6 space-y-0 mt-16 max-sm:mt-16 max-sm:space-y-0 max-sm:justify-center  sm:mt-16 sm:space-y-0  md:space-y-0  sm:bg-white sm:h-3/4 lg:bg-black sm:rounded-lg">
-                  {/* This div will be hidden on medium and larger screens */}
-
-
+              <div className="absolute max-sm:w-full w-3/5 inset-y-8 place-content-center mt-16 space-y-0 md:bg-black max-sm:bg-white md:rounded-none rounded-lg">
+                <div className="flex flex-col w- gap-6 space-y-0 mt-16 max-sm:mt-16 max-sm:space-y-0 max-sm:justify-center sm:mt-16 sm:space-y-0 md:space-y-0 sm:bg-white sm:h-3/4 lg:bg-black sm:rounded-lg">
                   <input
                     type="text"
                     placeholder="username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
-                    className=" lg:bg-black h-6 sm:placeholder:text-black sm:text-black focus:outline-none   w-5/5  text-sm text-white placeholder-white mt-20 border-b-2 max-sm:border-black   max-sm:mt-0 max-sm:w-4/5 max-sm:ml-6 max-sm:mr-6 max-sm:bg-white   placeholder:text-white max-sm:placeholder:bg-white max-sm:placeholder:text-black max-sm:text-black sm:mr-6 sm:ml-6 lg:text-white lg:placeholder:text-white sm:border-black lg:border-white"
+                    className=" lg:bg-black h-6 sm:placeholder:text-black sm:text-black focus:outline-none w-5/5 text-sm text-white placeholder-white mt-20 border-b-2 max-sm:border-black  max-sm:mt-0 max-sm:w-4/5 max-sm:ml-6 max-sm:mr-6 bg-transparent placeholder:text-white  max-sm:placeholder:text-black max-sm:text-black sm:mr-6 sm:ml-6 lg:text-white lg:placeholder:text-white"
                   />
-
                   <input
                     type="password"
                     placeholder="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="  border-b-2 h-6 lg:bg-black sm:text-black sm:placeholder:text-black    focus:outline-none   text-sm text-white max-sm:border-black   max-sm:mt-0 max-sm:w-4/5 max-sm:ml-6 max-sm:mr-6 max-sm:bg-white max-sm:text-black sm:mr-6 sm:ml-6  lg:text-white lg:placeholder:text-white sm:border-black lg:border-white"
+                    className="bg-transparent  border-b-2 h-6 lg:bg-black sm:text-black sm:placeholder:text-black focus:outline-none text-sm text-white max-sm:border-black  max-sm:mt-0 max-sm:w-4/5 max-sm:ml-6 max-sm:mr-6 max-sm:text-black max-sm:placeholder:text-black sm:mr-6 sm:ml-6 lg:text-white lg:placeholder:text-white  "
                   />
                   <div className="w-4/5 flex flex-col justify-start items-center mt-8 space-y-8"></div>
+
                   <button onClick={handleLogin} className="my-1 rounded-full bg-white  text-black font-bold py-2 px-4 w-full max-sm:w-4/5 max-sm:ml-6 max-sm:mr-6 max-sm:bg-[#1C1A1A]  max-sm:text-white sm:bg-black lg:bg-white sm:mr-6 sm:ml-6 sm:w-4/5 sm:text-white lg:text-black">
                     Log in
                   </button>
+
                   <div>
                     {successfulMessage && (
                       <p className="text-green-500 mt-2">{successfulMessage || 'successful'}</p>
@@ -126,19 +119,34 @@ const LoginPage = () => {
                   </div>
                   <div className="mt-2 text-sm flex items-center justify-center ">
                     <p className="text-black md:text-white"> Do not have an account yet? </p>
-                    <p className="text-blue-800 hover:underline cursor-pointer" onClick={openSignUpPage}>Sign up</p>
+                    <p className="text-[#4880FF] font-bold hover:underline cursor-pointer" onClick={openSignUpPage}>Sign up</p>
                   </div>
 
                 </div>
               </div>
             </div>
           </div>
-          <div className="  lg:text-white font-bold text-7xl absolute top-8  max-sm:text-5xl max-sm:mt-8 max-sm:hidden max-md:text-5xl max-sm:ml-4 md:text-transparent ml-96 left-60 sm:text-transparent md:hidden-block">
-            log
+
+          <div className="lg:text-white font-bold text-7xl absolute top-8 max-sm:text-5xl max-sm:mt-8 max-sm:hidden max-md:text-5xl max-sm:ml-4 md:text-transparent ml-96 left-60 sm:text-transparent md:hidden-block">
+            Log
           </div>
           {/* // White part for other content  */}
           <div className=" lg:text-black text-start font-bold text-7xl absolute inset-y-0 right-0 w-3/5 md:w-2/5 bg-white hidden md:block md:text-transparent" style={leftBorderRadius} >
-            <div className="lg:text-black mt-8 md:text-transparent">in</div>
+            <div className="lg:text-black mt-8 md:text-transparent">
+              in
+              <img
+                src={articleIcon}
+                alt="Article Icon"
+                className="hidden lg:inline h-8 w-8 ml-4"
+              />
+            </div>
+            <div className="flex w-full h-3/5 justify-center items-center">
+              <img
+                src={LoginAnimation}
+                alt="Login Animation"
+                className="inline h-[150px] w-[150px]"
+              />
+            </div>
           </div>
 
         </div>
--- src/pages/Authentification/SignUp.jsx
+++ src/pages/Authentification/SignUp.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import articleIcon from '../.././assets/icons/article.svg';
+import SignupAnimation from "../../assets/gifs/SignUpAnimation.gif"
+import { AiFillPicture } from "react-icons/ai";
 import { signUp } from "../../services/authApi";
 
 const SignUpPage = () => {
@@ -69,128 +71,133 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className='bg-black absolute right-0 top-0 w-full h-full grid grid-cols-2 '>
-      <div className="max-sm:mt-0 max-sm:hidd max-sm:justify-start absolute inset-y-0 right-0 w-3/5 md:w-3/5 bg-white max-sm:bg-white max-sm:w-full max-sm:h-full h-full md:h-full justify-center place-content-center space-y-6 lg:justify-center max-sm:rounded-none lg:rounded-s-[20px] md:rounded-s-[20px] sm:rounded-s-[20px] md:rounded-bl-lg">
-
-        <div className="lg:flex max-sm:grid max-sm:top-0 max-sm:left-0 top-0 max-sm:ml-8 max-sm:mt-4 max-sm:text-white text-sm max-sm:text-2xl mb-0 mt-10 ml-52 space-y-0 ">
-          <h1 className="max-sm:hidden text-black font-bold text-xl mb-8 lg:left-6"> Create Account </h1>
-          <img
-            src={articleIcon}
-            alt="Article Icon"
-            className="hidden md:inline-block lg:inline h-8 w-8 ml-4 lg:mt-0"
-          />
-
-          <h1 className="max-sm:left-0 top-0 text-black bg-white max-sm:mt-4 max-sm:text-4xl max-sm:font-bold lg:text-black"> Signup </h1>
-
-          <p className="max-sm:left-0 top-0 text-black bg-white max-sm:mt-0 max-sm:text-xl max-sm:font-bold lg:text-black">Welcome among us!</p>
-
-          <div className="max-sm:left-0  top-0 max-sm:mt-0 max-sm:ml-6 max-sm: text-white max-sm: font-bold  max-sm:text-xl   max-sm: mt-0 md:hidden sm-hidden lg:hidden max-md:text-5xl  ">
-
-          </div>
-        </div>
-
-        <div className="max-sm:justify-start mt-0 max-sm:text-white  text-sm max-sm:ml-0 max-sm:text-2xl max-sm:mt-6 mb-0 space-y-0">
-
-          <div className="flex w-72 flex-col gap-6 max-sm:hidden space-y-0 mt-16 max-sm:mt-16 max-sm:space-y-0 max-sm:justify-center  sm:mt-16 max-sm:h-full sm:space-y-0  md:space-y-0  sm:bg-white sm:h-3/4 lg:bg-black sm:rounded-lg">
-            {/* This div will be hidden on medium and larger screens */}
+    <div className='bg-black absolute right-0 top-0 w-full h-full justify-start items-center grid grid-cols-2 '>
+      <div className="flex h-3/5 justify-center items-center">
+        <img
+          src={SignupAnimation}
+          alt="Login Animation"
+          className="inline h-[150px] w-[150px]"
+        />
+      </div>
+      <div className="absolute inset-y-0 right-0 w-3/5 max-sm:w-full bg-white max-sm:bg-white place-content-center space-y-6 max-sm:rounded-none lg:rounded-s-[20px] md:rounded-s-[20px] sm:rounded-s-[20px] md:rounded-bl-lg">
+        <div className="flex flex-col items-center justify-center p-5 max-sm:p-10 md:p-0 mt-6 max-sm:text-white text-sm max-sm:text-2xl">
+          <div className="max-sm:hidden flex flex-row">
+            <h1 className="text-black font-bold text-xl mb-8"> Create Account </h1>
+            <img
+              src={articleIcon}
+              alt="Article Icon"
+              className="max-sm:hidden md:inline-block lg:inline h-8 w-8 ml-4 lg:mt-0"
+            />
           </div>
 
-          <div className="absolute inset-y-8  w-2/5 md:w-5/5  h-4 md:h-4/5 flex place-content-center mt-16 space-y-0 ">
-            <div className="flex w-72 flex-col max-sm:h-full gap-6 space-y-0  ">
+          <div className="absolute top-1 left-1">
+            <div className="hidden max-sm:flex flex-col items-start justify-start p-5 space-y-2">
+              <h1 className="text-black bg-white text-4xl font-bold"> Sign Up </h1>
+              <p className="text-black bg-white text-xl font-bold">Welcome among us!</p>
+            </div>
+          </div>
 
-              <div className="flex w-72 ml-32 flex-col gap-6 space-y-0 mt-16 max-sm:left-0 top-0 max-sm:mt-16 max-sm:space-y-0 max-sm:bg-black max-sm:justify-center max-sm:rounded-lg sm:mt-16 sm:space-y-0 md:space-y-0 sm:bg-white  lg:bg-white sm:rounded-lg">
-                {/* This div will be hidden on medium and larger screens */}
-                <label htmlFor="profilePicture" className="text-black text-sm mb-2">
-                  Choose Your Profile Picture
+          {/* <div className="absolute inset-y-8  w-2/5 md:w-5/5  h-4 md:h-4/5 flex place-content-center mt-16 space-y-0 "> */}
+          <div className="flex flex-col space-y-10 w-full md:w-4/5 lg:w-3/5 max-sm:mt-20">
+            <div className="flex flex-col space-y-8 max-sm:bg-black py-5 max-sm:justify-center max-sm:rounded-lg sm:bg-white lg:bg-white sm:rounded-lg">
+              <div className="flex flex-row w-full space-x-5">
+                <label
+                  htmlFor="profilePicture"
+                  className="border-2 border-[#434343] rounded-xl p-2 flex flex-row max-sm:text-white text-black font-semibold max-sm:text-sm text-md max-sm:ml-10 cursor-pointer"
+                >
+                  <AiFillPicture className="mt-1 mr-3 text-md" />
+                  Profile Picture
                 </label>
                 <input
                   type="file"
                   id="profilePicture"
                   name="profilePicture"
                   accept="image/*"
+                  placeholder="Enter PDF URL"
+                  style={{ display: 'none' }}
                   onChange={handleProfilePictureChange}
                 />
-                {profilePicture ? (
-                  <p className="text-black mt-2">
-                    Profile Picture: {profilePicture.name}
-                  </p>
-                ) : (
-                  <p className="text-black mt-2">
-                    You can upload a profile picture (optional)
-                  </p>
+              </div>
+
+              {/* {profilePicture ? (
+                <p className="text-black mt-2">
+                  Profile Picture: {profilePicture.name}
+                </p>
+              ) : (
+                <p className="text-black mt-2">
+                  You can upload a profile picture (optional)
+                </p>
+              )} */}
+
+              <input
+                type="text"
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className="h-6 focus:outline-none bg-white text-sm text-black placeholder-black mt-2 md:mt-14 border-b-2 border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
+              />
+
+              <input
+                type="text"
+                placeholder="Family Name"
+                value={familyName}
+                onChange={(e) => setFamilyName(e.target.value)}
+                className="h-6 focus:outline-none bg-white w-5/5 text-sm text-black placeholder-black mt-14 border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
+              />
+
+              <input
+                type="text"
+                placeholder="Username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                className="h-6 bg-white focus:outline-none  text-sm text-black placeholder-black  border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
+              />
+
+              <input
+                type="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="  h-6 bg-white focus:outline-none text-sm text-black placeholder-black  border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
+              />
+
+              <input
+                type="password"
+                placeholder="Password "
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="h-6 bg-white focus:outline-none text-sm text-black placeholder-black  border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
+              />
+
+              <div className="flex flex-col space-y-4">
+                {erreur && (
+                  <div className="text-red-500 font-semibold">
+                    {erreur || 'An error occurred'}
+                  </div>
                 )}
-                <input
-                  type="text"
-                  placeholder="First Name"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  className="  h-6  focus:outline-none bg-white w-5/5 text-sm text-black placeholder-black mt-14 border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
-                />
 
-                <input
-                  type="text"
-                  placeholder="Family Name"
-                  value={familyName}
-                  onChange={(e) => setFamilyName(e.target.value)}
-                  className="  h-6  focus:outline-none bg-white w-5/5 text-sm text-black placeholder-black mt-14 border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
-                />
+                {successfulMessage && (
+                  <div className="text-green-500 font-semibold">
+                    {successfulMessage || 'successful'}
+                  </div>
+                )}
 
-                <input
-                  type="text"
-                  placeholder="Username"
-                  value={userName}
-                  onChange={(e) => setUserName(e.target.value)}
-                  className="h-6 bg-white focus:outline-none  text-sm text-black placeholder-black  border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
-                />
-
-                <input
-                  type="email"
-                  placeholder="E-mail"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="  h-6 bg-white focus:outline-none text-sm text-black placeholder-black  border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
-                />
-
-                <input
-                  type="password"
-                  placeholder="Password "
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="  h-6 bg-white focus:outline-none   text-sm text-black placeholder-black  border-b-2  border-[#EAEAEA] max-sm:ml-10 max-sm:mr-6 max-sm:bg-black max-sm:placeholder:text-white max-sm:text-white"
-                />
-
-                <button className="my-1 rounded-full max-sm:text-base max-sm:font-bold max-sm:ml-8 bg-[#434343] hover:white text-white font-bold py-2 px-4 w-full  max-sm:mr-6 ml-6 max-sm:w-3/4 max-sm:bg-white max-sm:placeholder:text-white   max-sm:text-black" onClick={handleSignUp}>
+                <button
+                  className="my-1 rounded-full max-sm:text-base max-sm:font-bold max-sm:ml-8 bg-[#434343] hover:white text-white font-bold py-2 px-6 max-sm:mr-6 ml-6 max-sm:w-3/4 max-sm:bg-white max-sm:placeholder:text-white max-sm:text-black"
+                  onClick={handleSignUp}
+                >
                   Create an account
                 </button>
 
-                <div>
-                  {successfulMessage && (
-                    <p className="text-green-500 mt-2">{successfulMessage || 'successful'}</p>
-                  )}
-
-                  {erreur && (
-                    <p className="text-red-500 mt-2">
-                      {erreur || 'An error occurred'}
-                    </p>
-                  )}
-
-                </div>
-                <div className="mt-0 text-sm flex justify-center  ">
+                <div className=" text-sm flex justify-center">
                   <p className="text-black flex justify-center max-sm:mt-0 max-sm:text-white">Already have an account? </p>
                   <p className="text-[#707F65] font-bold hover:underline flex justify-center cursor-pointer" onClick={openLogInPage}>Login</p>
                 </div>
-
-                <div className="flex w-72 ml-32 flex-col gap-6 space-y-0 mt-16 max-sm:left-0 top-0 max-sm:mt-16 max-sm:space-y-0 max-sm:bg-white max-sm:justify-center max-sm:rounded-lg sm:mt-16 sm:space-y-0 md:space-y-0 sm:bg-white  lg:bg-white sm:rounded-lg">
-
-                </div>
-                <div className="flex w-72 ml-32 flex-col gap-6 space-y-0 mt-16 max-sm:left-0 top-0 max-sm:mt-16 max-sm:space-y-0 max-sm:bg-white max-sm:justify-center max-sm:rounded-lg sm:mt-16 sm:space-y-0 md:space-y-0 sm:bg-white  lg:bg-white sm:rounded-lg">
-
-                </div>
-                <div className="flex w-72 ml-32 flex-col gap-6 space-y-0 mt-16 max-sm:left-0 top-0 max-sm:mt-16 max-sm:space-y-0 max-sm:bg-white max-sm:justify-center max-sm:rounded-lg sm:mt-16 sm:space-y-0 md:space-y-0 sm:bg-white  lg:bg-white sm:rounded-lg">
-                </div>
               </div>
+
             </div>
           </div>
+          {/* </div> */}
         </div>
       </div>
     </div>
