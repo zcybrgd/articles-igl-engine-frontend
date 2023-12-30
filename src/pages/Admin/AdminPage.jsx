0d07c69aca4f144a604130f37ee908ce73d5ea05
Nwasler <107283332+NaDdjg@users.noreply.github.com>
2023-12-30 18:32:59 +0100

    added Footer

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import UploadSection from "./UploadSection";
 import LastEditSection from "./LastEditSection";
 import ModeratorsSection from "./ModeratorsSection";
+import Footer from '../../components/Shared/Footer/Footer';
 
 function AdminPage() {
     return (
-        <div className="flex flex-col space-y-20 mt-10">
-            {/* upload Section */}
-            <UploadSection />
+        <>
+            <div className="flex flex-col space-y-20 mt-10">
+                {/* upload Section */}
+                <UploadSection />
 
-            {/* last edit section  */}
-            <LastEditSection />
+                {/* last edit section  */}
+                <LastEditSection />
 
-            {/* moderators section */}
-            <ModeratorsSection />
-        </div>
+                {/* moderators section */}
+                <ModeratorsSection />
+            </div>
+
+            {/* Footer */}
+            <div className="app absolute right-0 left-0 w-full mt-32">
+                <Footer />
+            </div>
+        </>
     )
 }
 export default AdminPage;
