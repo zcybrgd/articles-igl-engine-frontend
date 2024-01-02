c5d47603cd6bd7383600f303b7a242d2978fac46
Nwasler <107283332+NaDdjg@users.noreply.github.com>
2024-01-02 18:15:03 +0100

    merged branch sidebar

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from 'react';
+import { HashLoader } from "react-spinners"
 
 const AuthContext = createContext({
     userRole: null,
@@ -10,24 +11,35 @@ const AuthContext = createContext({
 });
 
 const AuthProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(false);
     const [userRole, setUserRole] = useState("");
     const [userName, setUserName] = useState("");
     const [token, setToken] = useState("");
     const [id, setId] = useState(0)
+
     const login = async (userData) => {
+        setIsLoading(true)
         setUserRole(userData.userRole);
         setUserName(userData.userName);
         setToken(userData.token);
         setId(userData.id)
         await new Promise((resolve) => setTimeout(resolve, 1000));
+        setIsLoading(false)
     };
 
     const logout = () => {
         setUserRole(null);
     };
 
+    if (isLoading) {
+        return (
+            <div className="fixed top-0 right-0 left-0 bottom-0 flex items-center justify-center">
+                <HashLoader color="#707F65" />
+            </div>
+        );
+    }
     return (
-        <AuthContext.Provider value={{ userRole, userName, token, id , login, logout }}>
+        <AuthContext.Provider value={{ userRole, userName, token, id, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
