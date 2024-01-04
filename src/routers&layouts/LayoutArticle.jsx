5652fce071b066d61c9c1c79ca861c013445fda1
Nwasler <107283332+NaDdjg@users.noreply.github.com>
2024-01-04 17:48:04 +0100

    fixed sizes

--- src/routers&layouts/LayoutArticle.jsx
+++ src/routers&layouts/LayoutArticle.jsx
@@ -8,7 +8,7 @@ const LayoutArticle = ({ userRole, page }) => {
 
             {/* absolute top-0 left-0 right-0 */}
             <div className="absolute top-0 left-0 right-0 bg-[#707F65] justify-center items-center ">
-                <div className="flex items-center justify-center lg:ml-10 lg::p-10 mt-20 md:mt-10 ">
+                <div className="flex items-center justify-center mt-20 md:mt-10 ">
                     <Outlet />
                 </div>
             </div>
